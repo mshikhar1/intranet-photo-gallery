4a09370fc925aba9d079cb52549d7f9b5faff255
mshikhar1 <212057150+mshikhar1@users.noreply.github.com>
2026-08-13 09:35:08 +0530

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,1 +1,8 @@
-window.PHOTOS = [];
+window.PHOTOS = [
+  { src: "photos/photo1.svg", thumb: "photos/photo1.svg", caption: "Photo 1" },
+  { src: "photos/photo2.svg", thumb: "photos/photo2.svg", caption: "Photo 2" },
+  { src: "photos/photo3.svg", thumb: "photos/photo3.svg", caption: "Photo 3" },
+  { src: "photos/photo4.svg", thumb: "photos/photo4.svg", caption: "Photo 4" },
+  { src: "photos/photo5.svg", thumb: "photos/photo5.svg", caption: "Photo 5" },
+  { src: "photos/photo6.svg", thumb: "photos/photo6.svg", caption: "Photo 6" }
+];
